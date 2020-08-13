53640686f2fa41fdcf1ac11f3523c5eab8d006c9
Joshua <joshuamdk6@gmail.com>
2020-08-13 18:28:04 +1000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -227,7 +227,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
   
         if(row.every(index => squares[index].classList.contains('taken'))) {
-          score +=10
+          score +=100
           scoreDisplay.innerHTML = score
           row.forEach(index => {
             squares[index].classList.remove('taken')
@@ -244,7 +244,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //game over
     function gameOver() {
       if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-        scoreDisplay.innerHTML = 'end'
+        alert("Game Over")
+        window.location.reload();
         clearInterval(timerId)
       }
     }
